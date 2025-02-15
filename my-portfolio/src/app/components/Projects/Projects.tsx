96020af06a2c806cdcfad15019318a2c884fa26e
Daniel <daniel.lam3987@gmail.com>
2025-02-14 21:07:05 -0500

    Projects undo

--- my-portfolio/src/app/components/Projects/Projects.tsx
+++ my-portfolio/src/app/components/Projects/Projects.tsx
@@ -1,46 +1,9 @@
-import React, { useState, useEffect } from "react";
-import ProjectCard from "./ProjectCard";
-
-interface Project {
-  name: string;
-  startDate: string;
-  endDate: string;
-  description: string;
-  techStack: string[];
-  websiteLink: string;
-  sourceCode: string;
+const Projects = () => {
+    return(
+        <section className="h-screen w-full">
+            
+        </section>
+    );
 }
-
-const Projects: React.FC = () => {
-  const [projects, setProjects] = useState<Project[]>([]);
-
-  useEffect(() => {
-    // Dynamically import the JSON file
-    import("../data/projects.json")
-      .then((data) => {
-        setProjects(data); // Assuming `projects.json` is an array
-      })
-      .catch((error) => {
-        console.error("Error loading projects data:", error);
-      });
-  }, []);
-
-  return (
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {projects.map((project, index) => (
-        <ProjectCard
-          key={index}
-          name={project.name}
-          startDate={project.startDate}
-          endDate={project.endDate}
-          description={project.description}
-          techStack={project.techStack}
-          websiteLink={project.websiteLink}
-          sourceCode={project.sourceCode}
-        />
-      ))}
-    </div>
-  );
-};
 
 export default Projects;
